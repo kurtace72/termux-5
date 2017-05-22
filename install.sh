2042430d57406a3f45386a09d7ea959f1f219b59
Diogo Felix <diogo2fex@gmail.com>
2017-05-22 20:56:43 -0300

    Fixed zsh syntax highlighting

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ curl -fsLo $HOME/.tmux.conf https://cdn.rawgit.com/onlurking/termux/master/.term
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
 
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting.git
 
 curl -fsLo $HOME/.oh-my-zsh/themes/lambda-mod.zsh-theme https://cdn.rawgit.com/onlurking/termux/master/.termux/lambda-mod.zsh-theme
 
