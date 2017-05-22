099aa000662daa6624ee4dbfe9cf0b0967e93cf0
Diogo Felix <diogo2fex@gmail,com>
2017-05-22 18:20:45 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Termux Dotfiles
+
+Features:
+
+ - Base16 Eighties colorscheme
+ - zsh
+ - oh-my-zsh
+ - zsh syntax highlighting
+ - tmux
+
+# Requirements
+- curl
+```bash
+apt install -y curl
+```
+
+# Installation
+```bash
+bash -c "$(curl -fsSL https://git.io/vHIrd)"
+```
--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ curl -fsLo $HOME/.termux/colors.properties --create-dirs https://cdn.rawgit.com/
 
 curl -fsLo $HOME/.termux/font.ttf --create-dirs https://cdn.rawgit.com/onlurking/termux/master/.termux/font.ttf
 
+curl -fsLo $HOME/.tmux.conf https://cdn.rawgit.com/onlurking/termux/master/.termux/.tmux.conf
+
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
 
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins
@@ -21,7 +23,6 @@ curl -fsLo $HOME/.oh-my-zsh/themes/lambda-mod.zsh-theme https://cdn.rawgit.com/o
 curl -fsLo $HOME/.zshrc https://cdn.rawgit.com/onlurking/termux/master/.termux/.zshrc
 
 curl -fsLo $HOME/.profile https://cdn.rawgit.com/onlurking/termux/master/.termux/.profile
-
 
 chsh -s zsh
 
